4ec6e1f41f720cb6007fe0c87dfc8d2310bc92ce
way-zer <himc.wicp@gmail.com>
2025-12-14 22:27:50 +0800

    OC: optimize fonts update (picked from foo)

--- extensions/freetype/src/arc/freetype/FreeTypeFontGenerator.java
+++ extensions/freetype/src/arc/freetype/FreeTypeFontGenerator.java
@@ -723,16 +723,18 @@ public class FreeTypeFontGenerator implements Disposable{
             return glyph;
         }
 
+        //MDTX: Don't update fonts more than once per frame (from https://github.com/mindustry-antigrief/Arc/commit/fed63f365839b5d0d3e8b6fe02307c373360e36e)
+        private boolean queued;
+
         public void getGlyphs(GlyphRun run, CharSequence str, int start, int end, Glyph lastGlyph){
             if(packer != null) packer.setPackToTexture(true); // All glyphs added after this are packed directly to the texture.
             super.getGlyphs(run, str, start, end, lastGlyph);
-            if(dirty && !ignoreDirty){
-                //queuing font updates fixes a crash on iOS.
+
+            if(dirty && !queued){
+                queued = true;
                 Core.app.post(() -> {
-                    if(dirty){
-                        dirty = false;
-                        packer.updateTextureRegions(regions, parameter.minFilter, parameter.magFilter, parameter.genMipMaps);
-                    }
+                    dirty = queued = false;
+                    packer.updateTextureRegions(regions, parameter.minFilter, parameter.magFilter, parameter.genMipMaps);
                 });
             }
         }
